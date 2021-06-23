4b994cf49917a23d4c499495a9d78d71483d15f4
OwenDavies97 <ohdavies97@gmail.com>
2021-06-23 14:16:05 +0100

    started JS for projects section

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+function toggleDisplay () {
+  var x = document.getElementsByClassName("mixed-messages");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggleDisplay2 () {
+  var x = document.getElementsByClassName("excursion");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
